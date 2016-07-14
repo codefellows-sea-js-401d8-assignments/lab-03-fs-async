cf50e3538831801f2b36168f850ac0fe4832e1cf
Mars <marshallmiller92@gmail.com>
2016-07-14 12:15:08 -0700

    no working tests yet

--- lab-Marshall/test/test.js
+++ lab-Marshall/test/test.js
@@ -1,8 +1,8 @@
 const expect = require('chai').expect;
-const readFiles = require('../lib/readFiles');
+const makeReadFiles = require('../lib/readFiles');
 
-describe('index async test', function() {
+describe('index async test', function(){
   it('should log in order', function(){
-      expect()to.eql()
+    expect(true).to.eql(true);
   });
 });
