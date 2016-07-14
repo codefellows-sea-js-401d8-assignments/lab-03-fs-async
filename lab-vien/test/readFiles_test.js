3144a454191bf43bcc9ea480b041f89e52970557
Gin <vienlyt@gmail.com>
2016-07-14 12:54:02 -0700

    CLI order done

--- lab-vien/test/readFiles_test.js
+++ lab-vien/test/readFiles_test.js
@@ -6,10 +6,25 @@ console.log(output);
 
 var files = ['./lib/textfiles/text1.txt', './lib/textfiles/text4.txt', './lib/textfiles/text3.txt'];
 
+var correctOutput = ['text1\n', 'text4\n', 'text3\n'];
+
 describe('the readFiles function', function() {
   it('should read from the array of files in the input then output correspondingly', function(done) {
     read(files, function(stuff){
+      // stuff is supposed to be the output array
+      // testing output array length against input array length
       expect(stuff.length).to.eql(files.length);
+      done();
+    });
+  });
+});
+
+describe('the readFiles function', function() {
+  it('should read from the array of files in the input then output correspondingly', function(done) {
+    read(files, function(stuff){
+      stuff.forEach(function(item) {
+        expect(item).to.equal(correctOutput[stuff.indexOf(item)]);
+      });
       done();
     });
   });
