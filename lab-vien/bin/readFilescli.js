1d48661b52e567c70cbf0d6e0bc377ee7688cd51
Gin <vienlyt@gmail.com>
2016-07-14 13:03:14 -0700

    finalized

--- lab-vien/bin/readFilescli.js
+++ lab-vien/bin/readFilescli.js
@@ -1,9 +1,9 @@
 var read = require('../lib/readFiles');
 
-var readFilescli = exports = module.exports = function(fileArr, writeStream) {
+var readFilescli = exports = module.exports = function(fileArr, callback, writeStream) {
   writeStream = writeStream || process.stdout;
   var fileArgs = process.argv.slice(2);
-  read(fileArgs);
+  read(fileArgs, callback);
   // read(fileArgs).done(function() {
   //   console.log(fileData);
   // });
