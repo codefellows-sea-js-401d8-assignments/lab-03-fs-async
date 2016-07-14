ca7cf5b81ffbc1d1f4c038e0d8d2edbb5ac0f592
Aliza Pilisuk <apilisuk@gmail.com>
2016-07-14 11:42:47 -0700

    working on testing and able to export readFiles functions

--- lab-aliza/readFiles.js
+++ lab-aliza/readFiles.js
@@ -3,28 +3,33 @@ const fs = require('fs');
 const ee = new EventEmitter();
 const argsArr = process.argv.slice(2);
 
-var filesArr = ['./lib/one.txt', './lib/two.txt', './lib/three.txt'];
-
-ee.on('next file', function(data) {
-  if(data) console.log(data.toString());
-  if(filesArr.length === 0) return;
-
-  fs.readFile(filesArr.shift(), function(err, data) {
-    if(err) return console.log(err);
-    ee.emit('next file', data);
+var fileReader = module.exports = function() {
+  var filesArr = ['./lib/one.txt', './lib/two.txt', './lib/three.txt'];
+  ee.on('next file', function(data) {
+    if(data) console.log(data.toString());
+    if(filesArr.length === 0) return;
+    fs.readFile(filesArr.shift(), function(err, data) {
+      if(err) return console.log(err);
+      ee.emit('next file', data);
+    });
   });
-});
+  ee.emit('next file');
+};
 
-ee.on('next arg', function(data) {
-  if(data) console.log(data.toString());
-  if(argsArr.length === 0) return;
+var CLReader = module.exports = function() {
+  ee.on('next arg', function(data) {
+    if(data) console.log(data.toString());
+    if(argsArr.length === 0) return;
 
-  fs.readFile(argsArr.shift(), function(err, data) {
-    if(err) return console.log(err);
-    ee.emit('next arg', data);
+    fs.readFile(argsArr.shift(), function(err, data) {
+      if(err) return console.log(err);
+      ee.emit('next arg', data);
+    });
   });
-});
+  ee.emit('next arg');
+};
 
-if(process.argv[2]) ee.emit('next arg');
 
-if(process.argv[2] === undefined) ee.emit('next file');
+if(process.argv[2]) CLReader();
+
+if(process.argv[2] === undefined) fileReader();
