60110d38c3e136ac3c031ce3fcb9a1111ff7dd00
Steven Than <steventhan11@gmail.com>
2016-07-14 11:57:04 -0700

    unable to write test, need to get help from TA

--- lab-steventhan/lib/readFiles.js
+++ lab-steventhan/lib/readFiles.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 var EE = require('events');
 
-var ee = new EE();
-ee.files = process.argv.slice(2);
+var readFiles = {};
+readFiles.ee = new EE();
+readFiles.files = process.argv.slice(2);
 
-ee.on('readFileDone', (data) => {
+readFiles.ee.on('readFileDone', (data) => {
   if (data) console.log(data.toString());
-  if (ee.files.length === 0) return;
-  fs.readFile(ee.files.shift(), (err, data) => {
+  if (readFiles.files.length === 0) return;
+  fs.readFile(readFiles.files.shift(), (err, data) => {
     if (err) console.log(err);
-    ee.emit('readFileDone', data);
+    readFiles.ee.emit('readFileDone', data);
   });
 });
 
-ee.emit('readFileDone');
+readFiles.ee.emit('readFileDone');
 
-module.exports = ee;
+module.exports = readFiles;
--- lab-steventhan/test/readFilesTest.js
+++ lab-steventhan/test/readFilesTest.js
@@ -1,19 +1,20 @@
-const expect = require('chai').expect;
-const readFilesEE = require('../lib/readFiles');
+// const expect = require('chai').expect;
+const readFiles = require('../lib/readFiles');
 
 describe('readFile event emitter', () => {
-  before(() => {
-    this.argvBackup = process.argv;
-    process.argv = [null, null, './lib/file1.txt', './lib/file2.txt', './lib/file3.txt'];
-  });
-
-  after(() => {
-    process.argv = this.argvBackup;
-  });
+  // before(() => {
+  //   this.argvBackup = process.argv;
+  //   readFiles.files = [null, null, './lib/file1.txt', './lib/file2.txt', './lib/file3.txt'];
+  // });
+  //
+  // after(() => {
+  //   process.argv = this.argvBackup;
+  // });
 
   it('should read all the files passed in', () => {
-    // readFilesEE.emit('readFileDone');
-    console.log(readFilesEE.files);
-    // expect(readFilesEE.files).to.eql(3);
+    readFiles.files = ['./lib/file1.txt', './lib/file2.txt', './lib/file3.txt'];
+    console.log(readFiles);
+    readFiles.ee.emit('readFileDone');
+    // expect(readFiles.files).to.eql(3);
   });
 });
