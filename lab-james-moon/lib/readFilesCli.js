a810f70c5fafff14a590bed77afcd12922c27dd0
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-07-14 12:14:23 -0700

    made a function printContents to async print the contents of each file grabbed from process.argv

--- lab-james-moon/lib/readFilesCli.js
+++ lab-james-moon/lib/readFilesCli.js
@@ -1,7 +1,4 @@
 const fs = require('fs');
-var EE = require('events');
-
-var ee = new EE();
 
 var allFiles = function() {
   return process.argv.slice(2);
@@ -9,16 +6,14 @@ var allFiles = function() {
 
 var files = allFiles();
 
-ee.on('file-contents', function(data) {
-  if(data) console.log(data.toString());
-  if(files.length === 0) return;
-
-  process.nextTick(() => {
-    fs.readFile(files.shift(), function(err, data) {
-      if(err) return console.log(err);
-      ee.emit('file-contents', data);
+var printContents = function() {
+  for (var i = 0; i < files.length; i++) {
+    fs.readFile(files[i], function(err, data) {
+      if (err) return console.log(err);
+      console.log(data.toString());
+      if(files.length === 0) return;
     });
-  });
-});
+  }
+};
 
-ee.emit('file-contents');
+printContents();
