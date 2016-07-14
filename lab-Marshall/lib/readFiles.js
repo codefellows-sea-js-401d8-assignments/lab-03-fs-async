f72641ddd564d5e127a2cda0f9f20d561451449e
Mars <marshallmiller92@gmail.com>
2016-07-14 11:04:43 -0700

    makeReadFile function

---
+++ lab-Marshall/lib/readFiles.js
@@ -0,0 +1,24 @@
+//initial commit
+const fs = require('fs');
+const Event = require('events');
+const ee = new Event();
+
+ee.on('read', (data) => {
+  console.log('READ: ', data.toString());
+});
+
+function makeReadFile(txt){
+  return function(){
+    fs.readFile(txt, (err, data) => {
+      ee.emit('read', data);
+    });
+  };
+}
+
+var one = makeReadFile('../text/one.txt');
+var two = makeReadFile('../text/two.txt');
+var three = makeReadFile('../text/three.txt');
+
+one();
+two();
+three();
