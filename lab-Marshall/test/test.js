825f0098067e5882f1fd013ed69f098b2077a452
Mars <marshallmiller92@gmail.com>
2016-07-14 10:44:07 -0700

    hollow test

---
+++ lab-Marshall/test/test.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const index = require('../lib/index');
+
+describe('index async test', function() {
+  it('should log in order', function(){
+      expect()to.eql();
+  });
+});
